c63920e6930b1d51b7733119a95120797b479025
Liam Roberts <liamroberts300@gmail.com>
2019-03-30 14:47:18 +0000

    added turn function, compshot function and no go zone horizontal

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const section2 = document.querySelector('.grid-two')
 const gridWidth = 10
 const gridHeight = 10
 const shipLength = 3
-const numShips = 5
-let noGoStart
+// const numShips = 5
+let noGoTopLeft
 let noGoLeft
 let noGoRight
-let noGoEnd
+let noGoBottomLeft
 let noGoLength
 let grid1Divs
 let grid2Divs
@@ -17,6 +17,9 @@ let randomNum
 let column
 let row
 let horizOrVert
+let turn
+let compShot
+const compShotArray = []
 
 for(let i = 0; i<100; i++){
   grid1Divs = document.createElement('div')
@@ -29,54 +32,64 @@ for(let i = 0; i<100; i++){
 const grid1 = section1.querySelectorAll('div')
 const grid2 = section2.querySelectorAll('div')
 
+function compShotGener(){
+  compShot = Math.floor(Math.random() * 100)
+  compTurn()
+}
+
+function compTurn(){
+  if(turn % 2 === 1){
+    if(compShotArray.includes(compShot)){
+      compShotGener()
+    } else{
+      compShotArray.push(compShot)
+      turn ++
+      console.log(`comp ${turn} number`)
+      console.log(`comp shot = ${compShot} and the array = ${compShotArray}`)
+      hitOrMiss(grid1, compShot)
+    }
+  }
+}
+
 function placeCompShips(){
-  // for(let i = 0; i<numShips; i++) {
-    randomNumber()
-    horizontalOrVertical()
-    console.log((randomNum + 1) + ' ship number, ' + randomNum + ' index number, '+ column + ' column number, ' + row + ' row number, ' + horizOrVert + ' 1 = horizontal / 2 = vertical')
-  // }
+  randomNumber()
+  horizontalOrVertical()
+  console.log((randomNum + 1) + ' ship number, ' + randomNum + ' index number, '+ column + ' column number, ' + row + ' row number, ' + horizOrVert + ' 1 = horizontal / 2 = vertical')
 }
 
 function randomNumber(){
   randomNum = Math.floor(Math.random() * grid2.length)
-  // console.log('index = ' + randomNum)
 
   column = randomNum % gridWidth
-  // console.log('column = ' + (column + 1))
 
   row = Math.floor(randomNum/gridWidth)
-  // console.log('row = ' + (row + 1))
 }
 
 function horizontalOrVertical(){
   horizOrVert = 1
   // Math.ceil(Math.random() * 2)
-  // console.log(horizOrVert + ' 1 = horizontal / 2 = vertical')
 
   if(horizOrVert === 1){
-    // console.log('horizontal', horizOrVert)
     generateShipHorizontal()
   } else {
-    // console.log('vertical', horizOrVert)
     generateShipVertically()
   }
 }
 
 function generateShipHorizontal(){
   while((gridWidth - column) < shipLength){
-    randomNumber()
+    placeCompShips()
   }
-  // checkIfShipHorizontal()
+  noGoZoneHorizontal()
   for(let i = 0; i < shipLength; i++){
-    noGoZoneHorizontal()
+    grid2[randomNum + i].classList.remove('no-go')
     grid2[randomNum + i].classList.add('ship')
   }
 }
 
-
 function generateShipVertically(){
   while((gridHeight - row) < shipLength) {
-    randomNumber()
+    placeCompShips()
   }
   for(let i = 0; i < shipLength; i++){
     grid2[randomNum + (i * 10)].classList.add('ship')
@@ -84,72 +97,137 @@ function generateShipVertically(){
 }
 
 function noGoZoneHorizontal(){
-  noGoStart = (randomNum - gridWidth - 1)
-  noGoEnd = (randomNum + gridWidth - 1)
-  noGoLeft = randomNum -1
-  noGoRight = randomNum + shipLength
   noGoLength = shipLength + 2
 
-  grid2[noGoStart].classList.add('no-go')
-  grid2[noGoLeft].classList.add('no-go')
-  grid2[noGoRight].classList.add('no-go')
-  grid2[noGoEnd].classList.add('no-go')
+  noGoTopLeft = (randomNum - gridWidth - 1)
+  noGoBottomLeft = (randomNum + gridWidth - 1)
+  noGoLeft = randomNum - 1
+  noGoRight = randomNum + shipLength
 
-  grid2[randomNum + shipLength].classList.add('no-go')
-  if(noGoStart % gridWidth === gridWidth - 1){
-    noGoStart = 0
-    noGoLength --
+  // top left corner
+  if(randomNum === 0){
+    noGoLength -= 1
+    noGoBottomLeft = randomNum + gridWidth
+
+    grid2[noGoRight].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoBottomLeft + i].classList.add('no-go')
+    }
+    // bottom left corner
+  } else if(randomNum === 90){
+    noGoLength -= 1
+    noGoTopLeft = (randomNum - gridWidth)
+
+    grid2[noGoRight].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoTopLeft + i].classList.add('no-go')
+    }
+    // top right corner
+  } else if(randomNum === 7){
+    noGoLength -= 1
+    noGoBottomLeft = (randomNum + gridWidth - 1)
+    grid2[noGoLeft].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoBottomLeft + i].classList.add('no-go')
+    }
+    // bottom right corner
+  } else if(randomNum === 97){
+    noGoLength -= 1
+    noGoTopLeft = randomNum - gridWidth - 1
+    grid2[noGoLeft].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoTopLeft + i].classList.add('no-go')
+    }
+  // left hand side no go zone
+  } else if(randomNum % gridWidth === 0){
+    noGoLength--
+    noGoTopLeft = randomNum - gridWidth
+    noGoBottomLeft = randomNum + gridWidth
+    grid2[noGoRight].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoTopLeft + i].classList.add('no-go')
+      grid2[noGoBottomLeft + i].classList.add('no-go')
+    }
+  //   // right hand side no zone
+  } else if(noGoTopLeft % gridWidth === gridWidth - (shipLength + 1)){
+    noGoLength -= 1
+    grid2[noGoLeft].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoTopLeft + i].classList.add('no-go')
+      grid2[noGoBottomLeft + i].classList.add('no-go')
+    }
+    // bottom no go zone
+  } else if(noGoLeft > 90) {
+    grid2[noGoRight].classList.add('no-go')
+    grid2[noGoLeft].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoTopLeft + i].classList.add('no-go')
+    }
+    // top no go zone
+  } else if(noGoRight < 10) {
+    grid2[noGoRight].classList.add('no-go')
+    grid2[noGoLeft].classList.add('no-go')
+
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoBottomLeft + i].classList.add('no-go')
+    }
+  } else{
+    grid2[noGoRight].classList.add('no-go')
+    grid2[noGoLeft].classList.add('no-go')
+    for(let i=0; i < noGoLength; i++){
+      grid2[noGoTopLeft + i].classList.add('no-go')
+      grid2[noGoBottomLeft + i].classList.add('no-go')
+    }
   }
 }
-
-// length of no go = shiplength + 2
-// start of no go point will be index - width - 1
-//
-//if(start of no go % width === width - 1)
-// start = 0
-// length --
-//
-// start + length + index+10+ index+10 - length - index-10
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 grid1[8].classList.add('ship')
 grid1[18].classList.add('ship')
 grid1[28].classList.add('ship')
 
-function hitOrMiss(square){
-  if(square.classList.contains('ship')){
-    square.classList.remove('ship')
-    square.classList.add('hit')
-  } else if (!square.classList.contains('ship') && !square.classList.contains('hit')){
-    square.classList.add('miss')
-  } else {
-    return false
+function hitOrMiss(grid, index){
+  if(grid[index].classList.contains('ship')){
+    grid[index].classList.remove('ship')
+    grid[index].classList.add('hit')
+  } else if (!grid[index].classList.contains('ship') && !grid[index].classList.contains('hit') ||     grid[index].classList.contains('no-go')) {
+    grid[index].classList.remove('no-go')
+    grid[index].classList.add('miss')
   }
 }
 
 function gamePlay(){
+  turn = 0
+  console.log(turn)
   placeCompShips()
+  placeCompShips()
+  placeCompShips()
+  placeCompShips()
+  placeCompShips()
+  userTurn()
 
   // grab buttons
   grid1.forEach((square1, index) => {
     square1.addEventListener('click', () => {
       console.log('clicked', index)
-      hitOrMiss(square1)
     })
   })
+}
 
+function userTurn(){
   grid2.forEach((square2, index) => {
     square2.addEventListener('click', () => {
       console.log('clicked', index)
-      hitOrMiss(square2)
+      hitOrMiss(grid2, index)
+      turn++
+      console.log(`user ${turn} number`)
+      compShotGener()
     })
   })
 }
