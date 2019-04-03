dd23a72879e5410ab834d41c0480ae2237a5826b
Liam Roberts <liamroberts300@gmail.com>
2019-04-03 11:26:40 +0100

    started estimated guess

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const winAudio = new Audio('sounds/Oh-yeah-sound-effect.mp3')
 const loseAudio = new Audio('sounds/Sad-trombone.mp3')
 const gameEnd = new Audio('sounds/Game-ending-sound-effect.mp3')
 
+// Grids
 const grid1 = []
 const grid2 = []
 
@@ -30,9 +31,11 @@ let gameInPlay
 let userShipHorizontal = true
 let turn = 0
 let length
-let userOrientation
+let userOrientation = 1
 let userHit = 0
 let computerHit = 0
+let compLastShotHit
+let lastHit
 
 // CREATE GRID
 for(let i = 0; i<width ** 2; i++){
@@ -44,7 +47,6 @@ for(let i = 0; i<width ** 2; i++){
   section2.appendChild(grid2Div)
   grid2.push(grid2Div)
 }
-
 
 function getShipOrientation(){
   return Math.floor(Math.random() * 2) ? 1 : 10
@@ -62,7 +64,6 @@ function getShipArray(index, length, orientation){
 function checkIfCanLayShip(index, length, orientation, grid){
   if(orientation === 1 && width - (index % width) < length) return false
   if(orientation === 10 && width - Math.floor(index/width) < length)return false
-
 
   // loop through and check that all the squares do not contain ship or no-go
   for(let i = 0; i<length; i++) {
@@ -99,7 +100,6 @@ function getNoGoIndexes(shipIndexes, grid) {
     ) {
       noGoZoneIndexes.push(shipIndex - width + 1)
     }
-
     // bottom left
     if(
       shipIndex % width > 0 && Math.floor(shipIndex/width) < width - 1 &&
@@ -144,30 +144,33 @@ compShipLength.forEach((length) => {
   layShip(Math.floor(Math.random() * width ** 2), length, getShipOrientation(), grid2)
 })
 
+function hideThingsOnStart(){
+  section2.classList.toggle('hide')
+  shipList.classList.toggle('hide')
+  button.classList.toggle('hide')
+}
 // lay player ships
 function userPlaceShip(index){
   if(!userShipLength.includes(length)){
-    gameMessage.innerText = 'You have already layed that ship!'
+    gameMessage.innerText = 'Select a valid ship or space to continue!'
     return false
   }
-  if(!userShipHorizontal) userOrientation = 10
-  else userOrientation = 1
   gameMessage.innerText = 'Place your ships to begin!'
   layShip(index, length, userOrientation, grid1)
 }
+
 
 function removeFromArray(length){
   const indexOf = userShipLength.indexOf(length)
   userShipLength.splice(indexOf, 1)
   length = 0
   if(userShipLength.length === 0){
+    hideThingsOnStart()
     gameMessage.innerText = 'Take a shot!'
     gameInPlay = true
   }
 }
-
 // HIT OR MISS FUNCTIONS
-
 function hitCounter(grid){
   const total = 17
   if(grid === grid1){
@@ -179,22 +182,38 @@ function hitCounter(grid){
   else if(computerHit === total) lose()
 }
 
-//
+function hitSound(){
+  if(bang.currentTime > 0){
+    bang.currentTime = 0
+    bang.play()
+  } else bang.play()
+}
+
 function hitOrMiss(grid, index){
   if(grid[index].classList.contains('ship')){
-    grid[index].classList.remove('ship')
-    grid[index].classList.add('hit')
-    if(bang.currentTime > 0){
-      bang.currentTime = 0
-      bang.play()
-    } else bang.play()
-    checkIfDestroyed(grid, index)
-    hitCounter(grid)
+    hit(grid, index)
+
   } else if (!grid[index].classList.contains('ship') && !grid[index].classList.contains('hit') || grid[index].classList.contains('no-go')) {
-    grid[index].classList.remove('no-go')
-    grid[index].classList.add('miss')
-    grid[index].innerText = 'X'
+    miss(grid, index)
   }
+}
+
+function hit(grid, index){
+  grid[index].classList.remove('ship')
+  grid[index].classList.add('hit')
+  hitSound()
+  checkIfDestroyed(grid, index)
+  hitCounter(grid)
+  if(grid === grid1){
+    compLastShotHit = true
+    lastHit = index
+  }
+}
+
+function miss(grid, index){
+  grid[index].classList.remove('no-go')
+  grid[index].classList.add('miss')
+  grid[index].innerText = 'X'
 }
 
 function checkIfDestroyed(grid, index){
@@ -211,7 +230,6 @@ function checkIfDestroyed(grid, index){
     for (let col = 0; col< shipLocations[row].length; col++) {
       if (shipLocations[row][col] === index) {
         shipLocations[row].splice(col, 1)
-        console.log(shipLocations)
         updateIfDestroyed(row)
       }
     }
@@ -228,6 +246,25 @@ function updateIfDestroyed(row){
   }
 }
 
+function shipHover(grid1, index){
+  let shipHover
+  for(let i = 0; i<length; i++){
+    shipHover = grid1[index + (i*userOrientation)]
+    if(checkIfCanLayShip(index, length, userOrientation, grid1)){
+      shipHover.classList.add('hover')
+    }
+  }
+}
+
+function shipMouseLeave(grid1, index){
+  let shipHover
+  for(let i = 0; i<length; i++){
+    shipHover = grid1[index + (i*userOrientation)]
+    if(checkIfCanLayShip(index, length, userOrientation, grid1)){
+      shipHover.classList.remove('hover')
+    }
+  }
+}
 
 // USER SHOT
 function userShot(grid, index){
@@ -237,9 +274,15 @@ function userShot(grid, index){
   }
   hitOrMiss(grid2, index)
   turn++
-  computerShotIndex()
+  determineIfEstimateGuess()
 }
 
+function determineIfEstimateGuess(){
+  if(turn % 2 === 1){
+    if(!compLastShotHit) computerShotIndex()
+    else estimatedGuess()
+  }
+}
 // COMPUTER FUNCTIONS
 // Computer shot functions
 function computerShotIndex(){
@@ -248,23 +291,42 @@ function computerShotIndex(){
 }
 
 function compTurn(compShotIndex){
-  if(turn % 2 === 1){
-    if(computerShotArray.includes(compShotIndex)){
-      computerShotIndex()
-    } else{
-      computerShotArray.push(compShotIndex)
-      turn ++
-      hitOrMiss(grid1, compShotIndex)
-    }
+  computerShotIndex()
+  if(computerShotArray.includes(compShotIndex)){
+    computerShotIndex()
+  } else{
+    computerShotArray.push(compShotIndex)
+    turn ++
+    hitOrMiss(grid1, compShotIndex)
   }
 }
 
-function hideThings(){
-  shipList.style.display = 'none'
-  button.style.display = 'none'
-  section1.style.display = 'none'
-  section2.style.display = 'none'
-  gameMessage.style.display = 'none'
+
+function estimatedGuess(){
+  console.log(lastHit)
+  lastHit = false
+}
+
+// estimated guess, save last computer hit(run it when computer hits)
+// need to interupt computer turn function so that it doesnt do a new random number
+// array of next shots if in middle = -1, -width, 1, width
+// need to check computerShotArray if the next shots havent been shot already
+// splice any that have
+// fire at one of the indexs from next shot array
+// on next turn if hit, restart the function
+// if miss splice the next shot element and try again
+
+
+// user clicks their shot
+// increases turn by 1
+// calls shot gener fucntion
+// calls check shot gener function
+
+function hideThingsOnWin(){
+  section1.classList.toggle('hide')
+  section2.classList.toggle('hide')
+  gameMessage.classList.toggle('hide')
+
 }
 
 function winSpecialEffects(){
@@ -276,46 +338,66 @@ function winSpecialEffects(){
 
 function win(){
   gameInPlay = false
-  hideThings()
+  hideThingsOnWin()
   winSpecialEffects()
   winLoseMessage.innerText = 'You have won!!'
 }
 
 function lose(){
   gameInPlay = false
-  hideThings()
+  hideThingsOnWin()
   winLoseMessage.innerText = 'You have lost!!'
   loseAudio.play()
 }
 
 // grab buttons
-
 grid1.forEach((square1, index) => {
   square1.addEventListener('click', () => {
     userPlaceShip(index)
+  })
+  square1.addEventListener('mouseover', () =>{
+    shipHover(grid1, index)
+  })
+  square1.addEventListener('mouseout', () =>{
+    shipMouseLeave(grid1, index)
   })
 })
 
 grid2.forEach((square2, index) => {
   square2.addEventListener('click', () => {
-    if(gameInPlay)userShot(grid2, index)
+    if(gameInPlay && turn % 2 === 0){
+      userShot(square2, index)
+    }
   })
 })
 
 button.addEventListener('click', () => {
   if(userShipHorizontal){
     userShipHorizontal = false
+    userOrientation = 10
     button.innerText = 'Vertical'
   } else{
     userShipHorizontal = true
+    userOrientation = 1
     button.innerText = 'Horizontal'
   }
 })
 
+// if userShipLength array does not contain shipbutton.value as number
+// do something to the button
+
+function placedShipButton(shipButton){
+  const placedShipButton = parseInt(shipButton.value)
+  console.log(placedShipButton)
+  if(!userShipLength.includes(placedShipButton)){
+    shipButton.style.color = 'green'
+  }
+}
+
 shipButton.forEach(shipButton => {
   shipButton.addEventListener('click', (e) => {
     length = parseInt(e.target.value)
-    console.log(length)
     selectedShip.innerText = shipButton.innerText
+    placedShipButton(shipButton)
   })
 })
